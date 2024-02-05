d2c47f94efdefb95e5c9d413a9748caf0857d34d
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-02-04 21:07:29 -0600

    fix

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,13 +3,13 @@ a = 10
 b = 5
 
 # Import functions from calculator_1.py
-from calculator_1 import add, subtract, multiply, divide
+from calculator_1 import add, sub, mul, div
 
 # Call each of the imported functions with arguments a and b
 result_add = add(a, b)
-result_subtract = subtract(a, b)
-result_multiply = multiply(a, b)
-result_divide = divide(a, b)
+result_subtract = sub(a, b)
+result_multiply = mul(a, b)
+result_divide = div(a, b)
 
 # Print the results
 print("Result of adding {} and {} is: {}".format(a, b, result_add))
