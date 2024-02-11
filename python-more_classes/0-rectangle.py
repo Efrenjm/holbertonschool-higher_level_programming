13f9b264650473558d6f75d176393162c980abe7
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-02-11 13:16:51 -0600

    new

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,10 +1,2 @@
 class Rectangle:
-
-    def area(self):
-        return self.length * self.width
-
-    def perimeter(self):
-        return 2 * (self.length + self.width)
-
-    def is_square(self):
-        return self.length == self.width
+	pass
