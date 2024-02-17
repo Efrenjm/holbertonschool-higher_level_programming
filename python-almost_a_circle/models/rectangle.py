025d67039be0ed1879943aa81acf0ab3600f7e1e
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-02-17 14:32:29 -0600

    something

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -46,24 +46,40 @@ class Rectangle(Base):
         self.__y = y
 
     def _validate_width(self, value):
+        """
+        validate width
+        """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
 
     def _validate_height(self, value):
+        """
+        validate width
+        """
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
 
     def _validate_x(self, value):
+        """
+        validate width
+        """
+
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
 
     def _validate_y(self, value):
+        """
+        validate width
+        """
+
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
