9b4af30abe297b386a03a038b4ba1176f288f899
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-02-17 10:44:33 -0600

    add python input output excercises

---
+++ python-classes/1-square.js
@@ -0,0 +1,18 @@
+// #!/usr/bin/python3
+// """defines a square with size"""
+
+
+// class Square:
+//     """a square class"""
+
+//     def __init__(self, size):
+//         self.__size = size
+    
+    
+InitInput = {"SelectedDate":0}
+EndInput = {"SelectedDate":0}
+TimeUnit = {"Days": ""}
+function DateDiff(init,end,format){}
+
+DateDiff(InitInput.SelectedDate, EndInput.SelectedDate, TimeUnit.Days)
+
