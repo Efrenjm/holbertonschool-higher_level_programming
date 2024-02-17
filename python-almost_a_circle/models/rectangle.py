603e6abcf6b5fb5262f6a28b6603089c2b39e34e
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-02-17 14:26:36 -0600

    fix the result

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -60,15 +60,15 @@ class Rectangle(Base):
 
     def _validate_x(self, value):
         if not isinstance(value, int):
-            raise TypeError("x-coordinate must be an integer")
+            raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x-coordinate must be >= 0")
+            raise ValueError("x must be >= 0")
 
     def _validate_y(self, value):
         if not isinstance(value, int):
-            raise TypeError("y-coordinate must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y-coordinate must be >= 0")
+            raise ValueError("y must be >= 0")
 
     @property
     def width(self):
