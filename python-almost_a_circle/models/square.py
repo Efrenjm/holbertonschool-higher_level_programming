73c7d57bc8902b377e186c6e27ae6f99180e2727
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-02-18 10:12:12 -0600

    add setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -49,3 +49,45 @@ class Square(Rectangle):
         """
 
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.__size}"
+
+    @property
+    def size(self):
+        """
+        Gets the current side length of the square.
+
+        Returns:
+            int: The square's side length.
+        """
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the side length of the square, updating both width and height.
+
+        Args:
+            value (int): The new side length for the square. Must be greater than 0.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not positive.
+        """
+
+        # if not isinstance(value, int):
+        #     raise TypeError("Size must be an integer.")
+        self._validate_width(value)
+        self.__size = value
+        self.__width = value
+        self.__height = value
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square object.
+
+        Returns:
+            str: A formatted string containing the square's class, ID, coordinates,
+                 and size.
+        """
+
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.__size}"
